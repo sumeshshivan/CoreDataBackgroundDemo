586cc875dff623657cb92f522da251c9915b8666
sumesh <sumesh@qburst.com>
2016-03-17 10:22:28 +0530

    Initial Commit

---
+++ CoreDataBackground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataBackground
+//
+//  Created by Sumesh on 17/03/16.
+//  Copyright © 2016 Sumesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataBackground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataBackground
+//
+//  Created by Sumesh on 17/03/16.
+//  Copyright © 2016 Sumesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
